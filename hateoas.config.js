07c051695e47b8816c58b52872e15ebbba1c1dc5
Vivek Bose <waivek@gmail.com>
2024-04-16 19:48:37 +0530

    (auto-commit)

--- hateoas.config.js
+++ hateoas.config.js
@@ -1,2 +1,11 @@
-{
+module.exports = {
+    apps: [{
+        name: 'hateoas',
+        cmd: 'python',
+        script: 'hateoas-api.py',
+        cwd: '/home/vivek/hateoas',
+        env: {
+            PATH: "/home/vivek/hateoas/.direnv/python-3.10.12/bin"
+        }
+    }],
 }
