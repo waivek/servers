4e2fefe582efb35a19b1f409d53fbb27539a00de
Vivek Bose <waivek@gmail.com>
2024-06-09 12:51:40 +0530

    (auto-commit)

---
+++ ui-components-css.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [{
+        name: 'ui-components-css',
+        cmd: 'python',
+        script: 'ui-components-css-api.py',
+        cwd: '/home/vivek/ui-components-css',
+        env: {
+            PATH: "/home/vivek/ui-components-css/.direnv/python-3.10.12/bin"
+        }
+    }],
+}
