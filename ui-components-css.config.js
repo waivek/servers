aefd9664ac2066a44cdae7b3c9ec74196b9d3a4f
Vivek Bose <waivek@gmail.com>
2024-06-13 14:44:39 +0530

    (auto-commit)

--- ui-components-css.config.js
+++ ui-components-css.config.js
@@ -1,11 +1,18 @@
 module.exports = {
-    apps: [{
-        name: 'ui-components-css',
-        cmd: 'python',
-        script: 'ui-components-css-api.py',
-        cwd: '/home/vivek/ui-components-css',
-        env: {
-            PATH: "/home/vivek/ui-components-css/.direnv/python-3.10.12/bin"
+    apps: [
+        {
+            name: 'ui-components-css',
+            cmd: 'python',
+            script: 'ui-components-css-api.py',
+            cwd: '/home/vivek/ui-components-css',
+            env: { PATH: "/home/vivek/ui-components-css/.direnv/python-3.10.12/bin" },
+            namespace: 'ui-components-css',
+        },
+        {
+            name: 'ui-components-css-tailwind-watch',
+            cmd: 'npx tailwindcss -o ~/ui-components-css/static/css-tailwind-outputs/ui-components-css-tailwind-output.css --watch',
+            cwd: '/home/vivek/ui-components-css',
+            namespace: 'ui-components-css',
         }
-    }],
+    ],
 }
